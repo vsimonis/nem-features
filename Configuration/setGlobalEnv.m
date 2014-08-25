819cdb53a5609ce964c165cb25174afca2c33155
vsimonis <valerie.simonis@gmail.com>
2014-08-25 16:28:39 -0500

    medix3

--- Configuration/setGlobalEnv.m
+++ Configuration/setGlobalEnv.m
@@ -60,12 +60,22 @@ function [ globalEnv ] = setGlobalEnv(  )
 % globalEnv.EstArea = 640;
 
 %% Medix 7
+% globalEnv.CodeDirectory= 'C:\Users\vsimonis\Documents\MATLAB\nem-features\';
+% globalEnv.WorkingDir= 'C:\Users\vsimonis\Documents\MATLAB\Data\N2_no_food-Wed\';
+% globalEnv.StudyInstanceName= 'Prelim';
+% globalEnv.VideoInputDir= 'C:\Users\vsimonis\Documents\MATLAB\Data\N2_no_food-Wed\';
+% globalEnv.VideoInputName= 'N2_no_food-Wed_13_Aug_2014-103129.avi';
+% globalEnv.LogFileName= 'N2_no_food-Wed_13_Aug_2014-102725.log';
+% globalEnv.EndFrame = 1000;
+% globalEnv.EstArea = 640;
+
+%% Medix 3
 globalEnv.CodeDirectory= 'C:\Users\vsimonis\Documents\MATLAB\nem-features\';
-globalEnv.WorkingDir= 'C:\Users\vsimonis\Documents\MATLAB\Data\N2_no_food-Wed\';
+globalEnv.WorkingDir= 'C:\Users\vsimonis\Documents\MATLAB\Data\n2_no_food2\';
 globalEnv.StudyInstanceName= 'Prelim';
-globalEnv.VideoInputDir= 'C:\Users\vsimonis\Documents\MATLAB\Data\N2_no_food-Wed\';
-globalEnv.VideoInputName= 'N2_no_food-Wed_13_Aug_2014-103129.avi';
-globalEnv.LogFileName= 'N2_no_food-Wed_13_Aug_2014-102725.log';
+globalEnv.VideoInputDir= 'C:\Users\vsimonis\Documents\MATLAB\Data\n2_no_food2\';
+globalEnv.VideoInputName= 'n2_no_food2-Wed_13_Aug_2014-124318.avi';
+globalEnv.LogFileName= 'n2_no_food2-Wed_13_Aug_2014-124258.log';
 globalEnv.EndFrame = 1000;
 globalEnv.EstArea = 640;
 end
